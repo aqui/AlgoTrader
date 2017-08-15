09c331aed4764e91cb8d439a0f0c6461a5f8f757
aqui <akifbatur@protonmail.com>
2017-08-15 23:23:56 +0300

    sth

--- src/main/java/com/algotrader/Main.java
+++ src/main/java/com/algotrader/Main.java
@@ -6,36 +6,37 @@ import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 import org.json.JSONObject;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class Main
-{
-	public static void main(String[] args) throws Exception
-	{
-//		int daysBefore = 1;
-//		long start = ZonedDateTime.now(ZoneOffset.UTC).minusDays(daysBefore).toEpochSecond();
-//		long end = ZonedDateTime.now(ZoneOffset.UTC).minusDays(0).toEpochSecond();
-//		//300: 5dk, 900: 15dk, 1800: 30dk, 7200: 2sa, 14400: 4sa, 86400: 1gun
-//		long period = 300;
-//		String currencyPair = "BTC_ETH";
-//		String chartDataURI = "https://poloniex.com/public?command=returnChartData&currencyPair="+currencyPair+"&start="+start+"&end="+end+"&period="+period;
-//		System.out.println(chartDataURI);
-//		PoloniexClient poloniexClient = new PoloniexClient(chartDataURI);
-//		poloniexClient.open();
-//		while(true)
-//		{
-//			
-//		}
-		//Bittrex
-		String apikey = "";
-		String secret = "";
-		Bittrex bittrex = new Bittrex(apikey, secret, 1, 1);
-		JSONObject json = new JSONObject(bittrex.getMarketSummary("btc-burst"));
-		double last = (double)json.getJSONArray("result").getJSONObject(0).get("Last");
-		System.out.printf("%.8f", last);
+public class Main {
+	public static void main(String[] args) throws Exception {
+		// int daysBefore = 1;
+		// long start =
+		// ZonedDateTime.now(ZoneOffset.UTC).minusDays(daysBefore).toEpochSecond();
+		// long end = ZonedDateTime.now(ZoneOffset.UTC).minusDays(0).toEpochSecond();
+		// //300: 5dk, 900: 15dk, 1800: 30dk, 7200: 2sa, 14400: 4sa, 86400: 1gun
+		// long period = 300;
+		// String currencyPair = "BTC_ETH";
+		// String chartDataURI =
+		// "https://poloniex.com/public?command=returnChartData&currencyPair="+currencyPair+"&start="+start+"&end="+end+"&period="+period;
+		// System.out.println(chartDataURI);
+		// PoloniexClient poloniexClient = new PoloniexClient(chartDataURI);
+		// poloniexClient.open();
+		// while(true)
+		// {
+		//
+		// }
+		// Bittrex
+		Timer time = new Timer();
+		ScheduledTask st = new ScheduledTask();
+		time.schedule(st, 0, 1000);
 	}
 }
---
+++ src/main/java/com/algotrader/ScheduledTask.java
@@ -0,0 +1,71 @@
+package com.algotrader;
+
+import java.util.TimerTask;
+
+import org.json.JSONObject;
+
+public class ScheduledTask extends TimerTask 
+{
+	public static double lastValue;
+	public static double newValue;
+	public static double boughtAt = 0.00111879d;
+	public static double totalBTC = 0.00000003d;
+	public static double totalWAVES = 10d;
+	public static double limitWAVES = 10d;
+	
+	public void run() 
+	{
+		String apikey = "831bc09194794e52bbff18a2fbc7ccc5";
+		String secret = "99cdcd3469b342b4a46d8623f80d0cdd";
+		Bittrex bittrex = new Bittrex(apikey, secret, 1, 1);
+		JSONObject json = new JSONObject(bittrex.getMarketSummary("btc-waves"));
+		if(newValue==0)
+		{
+			newValue = (double) json.getJSONArray("result").getJSONObject(0).get("Last");
+			lastValue = (double) json.getJSONArray("result").getJSONObject(0).get("Last");
+			if(newValue > boughtAt)
+				sell();
+		}
+		else
+		{
+			if(newValue != (double) json.getJSONArray("result").getJSONObject(0).get("Last"))
+			{
+				lastValue = newValue;
+				newValue = (double) json.getJSONArray("result").getJSONObject(0).get("Last");
+				if(newValue > lastValue && totalWAVES > 0)
+					sell();
+				if(newValue < lastValue && totalBTC > 0)
+					buy();
+			}
+		}
+	}
+
+	private void sell() 
+	{
+		System.out.println("sold");
+		double satis = newValue * totalWAVES;
+		satis = satis - (satis * 0.25) / 100;
+		totalBTC += satis;
+		totalWAVES = 0;
+		printEmAll();
+	}
+	
+	private void buy() 
+	{
+		System.out.println("bought");
+		double alis = totalBTC / newValue;
+		alis = alis - (alis * 0.25) / 100;
+		totalWAVES += alis;
+		totalBTC = 0;
+		printEmAll();
+	}
+	
+	private void printEmAll()
+	{
+		System.out.printf("new: %.8f\n", newValue);
+		System.out.printf("last: %.8f\n", lastValue);
+		System.out.printf("BTC: %.8f\n", totalBTC);
+		System.out.printf("WAVES: %.8f\n", totalWAVES);
+		System.out.println("--------------------------------");
+	}
+}
