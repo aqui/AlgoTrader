31976c6a7fa8a1110f9fadac11df0530becf4be1
aqui <akifbatur@protonmail.com>
2017-08-15 21:15:48 +0300

    Bittrex

--- src/main/java/com/algotrader/Main.java
+++ src/main/java/com/algotrader/Main.java
@@ -1,25 +1,41 @@
 package com.algotrader;
 
+import java.net.URL;
+import java.text.DecimalFormat;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+import java.util.Iterator;
+import java.util.List;
+
+import org.json.JSONObject;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Main
 {
 	public static void main(String[] args) throws Exception
 	{
-		int daysBefore = 1;
-		long start = ZonedDateTime.now(ZoneOffset.UTC).minusDays(daysBefore).toEpochSecond();
-		long end = ZonedDateTime.now(ZoneOffset.UTC).minusDays(0).toEpochSecond();
-		//300: 5dk, 900: 15dk, 1800: 30dk, 7200: 2sa, 14400: 4sa, 86400: 1gun
-		long period = 300;
-		String currencyPair = "BTC_ETH";
-		String chartDataURI = "https://poloniex.com/public?command=returnChartData&currencyPair="+currencyPair+"&start="+start+"&end="+end+"&period="+period;
-		System.out.println(chartDataURI);
-		PoloniexClient poloniexClient = new PoloniexClient(chartDataURI);
-		poloniexClient.open();
-		while(true)
-		{
-			
-		}
+//		int daysBefore = 1;
+//		long start = ZonedDateTime.now(ZoneOffset.UTC).minusDays(daysBefore).toEpochSecond();
+//		long end = ZonedDateTime.now(ZoneOffset.UTC).minusDays(0).toEpochSecond();
+//		//300: 5dk, 900: 15dk, 1800: 30dk, 7200: 2sa, 14400: 4sa, 86400: 1gun
+//		long period = 300;
+//		String currencyPair = "BTC_ETH";
+//		String chartDataURI = "https://poloniex.com/public?command=returnChartData&currencyPair="+currencyPair+"&start="+start+"&end="+end+"&period="+period;
+//		System.out.println(chartDataURI);
+//		PoloniexClient poloniexClient = new PoloniexClient(chartDataURI);
+//		poloniexClient.open();
+//		while(true)
+//		{
+//			
+//		}
+		//Bittrex
+		String apikey = "";
+		String secret = "";
+		Bittrex bittrex = new Bittrex(apikey, secret, 1, 1);
+		JSONObject json = new JSONObject(bittrex.getMarketSummary("btc-burst"));
+		double last = (double)json.getJSONArray("result").getJSONObject(0).get("Last");
+		System.out.printf("%.8f", last);
 	}
 }
