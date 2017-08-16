f98e8ea4b6ae60fb037af35003c88af52e998b43
akifbatur <akifbatur@bilisim.com.tr>
2017-08-16 17:48:54 +0300

    sth

--- src/main/java/com/algotrader/ScheduledTask.java
+++ src/main/java/com/algotrader/ScheduledTask.java
@@ -8,23 +8,27 @@ public class ScheduledTask extends TimerTask
 {
 	public static double lastValue;
 	public static double newValue;
-	public static double boughtAt = 0.00111879d;
-	public static double totalBTC = 0.00000003d;
-	public static double totalWAVES = 10d;
-	public static double limitWAVES = 10d;
+	public static double boughtAt = 0.01049500d;
+	public static double totalBTC = 0.0000000d;
+	public static double totalWAVES = 3d;
+	public static double preWAVES;
+	public static double preBTC;
 	
 	public void run() 
 	{
 		String apikey = "asd";
 		String secret = "asd";
 		Bittrex bittrex = new Bittrex(apikey, secret, 1, 1);
-		JSONObject json = new JSONObject(bittrex.getMarketSummary("btc-waves"));
+		JSONObject json = new JSONObject(bittrex.getMarketSummary("btc-neo"));
+		double current = (double) json.getJSONArray("result").getJSONObject(0).get("Last");
 		if(newValue==0)
 		{
-			newValue = (double) json.getJSONArray("result").getJSONObject(0).get("Last");
-			lastValue = (double) json.getJSONArray("result").getJSONObject(0).get("Last");
+			newValue = current;
+			lastValue = current;
 			if(newValue > boughtAt)
+			{
 				sell();
+			}
 		}
 		else
 		{
@@ -33,30 +37,46 @@ public class ScheduledTask extends TimerTask
 				lastValue = newValue;
 				newValue = (double) json.getJSONArray("result").getJSONObject(0).get("Last");
 				if(newValue > lastValue && totalWAVES > 0)
-					sell();
-				if(newValue < lastValue && totalBTC > 0)
 					buy();
+				if(newValue < lastValue && totalBTC > 0)
+					sell();
 			}
 		}
 	}
 
 	private void sell() 
 	{
-		System.out.println("sold");
+		preWAVES = totalWAVES;
 		double satis = newValue * totalWAVES;
 		satis = satis - (satis * 0.25) / 100;
-		totalBTC += satis;
-		totalWAVES = 0;
+		if(totalBTC < satis)
+		{
+			totalBTC += satis;
+			totalWAVES = 0;
+		}
+		else
+		{
+			return;
+		}
+		System.out.println("sold");
 		printEmAll();
 	}
 	
 	private void buy() 
 	{
-		System.out.println("bought");
+		preBTC = totalBTC;
 		double alis = totalBTC / newValue;
 		alis = alis - (alis * 0.25) / 100;
-		totalWAVES += alis;
-		totalBTC = 0;
+		if(preWAVES < alis)
+		{
+			totalWAVES += alis;
+			totalBTC = 0;
+		}
+		else
+		{
+			return;
+		}
+		System.out.println("bought");
 		printEmAll();
 	}
 	
@@ -65,7 +85,7 @@ public class ScheduledTask extends TimerTask
 		System.out.printf("new: %.8f\n", newValue);
 		System.out.printf("last: %.8f\n", lastValue);
 		System.out.printf("BTC: %.8f\n", totalBTC);
-		System.out.printf("WAVES: %.8f\n", totalWAVES);
+		System.out.printf("NEO: %.8f\n", totalWAVES);
 		System.out.println("--------------------------------");
 	}
 }
