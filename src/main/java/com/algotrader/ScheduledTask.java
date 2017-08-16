46280f88af7110c1e2a8f08c12285b90382e7ee8
aqui <akifbatur@protonmail.com>
2017-08-16 20:45:03 +0300

    test

--- src/main/java/com/algotrader/ScheduledTask.java
+++ src/main/java/com/algotrader/ScheduledTask.java
@@ -8,19 +8,26 @@ public class ScheduledTask extends TimerTask
 {
 	public static double lastValue;
 	public static double newValue;
-	public static double boughtAt = 0.01049500d;
+	public static double boughtAt = 0.1d;
 	public static double totalBTC = 0.0000000d;
-	public static double totalWAVES = 3d;
+	public static double totalWAVES = 10;
 	public static double preWAVES;
 	public static double preBTC;
+	public static int i = 0;
 	
 	public void run() 
 	{
 		String apikey = "asd";
 		String secret = "asd";
-		Bittrex bittrex = new Bittrex(apikey, secret, 1, 1);
-		JSONObject json = new JSONObject(bittrex.getMarketSummary("btc-neo"));
-		double current = (double) json.getJSONArray("result").getJSONObject(0).get("Last");
+//		Bittrex bittrex = new Bittrex(apikey, secret, 1, 1);
+//		JSONObject json = new JSONObject(bittrex.getMarketSummary("btc-neo"));
+		//double current = (double) json.getJSONArray("result").getJSONObject(0).get("Last");
+		double current;
+		if(i%2==0)
+			current = 0.2d;
+		else
+			current = 0.1d;
+		i++;
 		if(newValue==0)
 		{
 			newValue = current;
@@ -32,14 +39,14 @@ public class ScheduledTask extends TimerTask
 		}
 		else
 		{
-			if(newValue != (double) json.getJSONArray("result").getJSONObject(0).get("Last"))
+			if(newValue != current)
 			{
 				lastValue = newValue;
-				newValue = (double) json.getJSONArray("result").getJSONObject(0).get("Last");
+				newValue = current;
 				if(newValue > lastValue && totalWAVES > 0)
-					buy();
-				if(newValue < lastValue && totalBTC > 0)
 					sell();
+				if(newValue < lastValue && totalBTC > 0)
+					buy();
 			}
 		}
 	}
@@ -56,6 +63,7 @@ public class ScheduledTask extends TimerTask
 		}
 		else
 		{
+			System.out.println("not sold: "+(totalBTC-satis));
 			return;
 		}
 		System.out.println("sold");
@@ -74,6 +82,7 @@ public class ScheduledTask extends TimerTask
 		}
 		else
 		{
+			System.out.println("not bought: "+(alis-preWAVES));
 			return;
 		}
 		System.out.println("bought");
